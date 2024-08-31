4bbb2e6458686a0d9c18b11626828c3dc4a6631d
faizanraza <faizr6304@gmail.com>
2024-08-31 20:10:38 +0500

    basic files setup

--- Vendar-web-vue/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
-import 'preline';
-
-createApp(App).use(router).mount('#app');
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+   <header class=" flex justify-between bg-white shadow-lg">
+        <div class="text-3xl font-bold text-primary-800 cursor-pointer">
+            <router-link to="/">Rigrex</router-link>
+        </div>
+   </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped></style>
