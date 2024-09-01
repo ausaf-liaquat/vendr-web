b28f4baf3e9ea53d739dfe98ff44734be1f7481c
faizanraza <faizr6304@gmail.com>
2024-09-02 00:37:00 +0500

    services section completed

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="w-full lg:6/12 md:w-6/12">
-                    <div class="w-[80%]">
+                    <div class="w-full lg:w-[80%]">
                         <h3 class="text-3xl font-semibold text-secondary-800" data-aos="flip-up" data-aos-duration="2000">Utilities</h3>
                         <p class="text-gray-700 mt-2 text-lg" data-aos="flip-up" data-aos-duration="2000">We provide an easy-to-use vending platform and distribution network for STS meters that give you the flexibility to manage your spaces. Streamline meter readings, automate billing and get transparency into consumption.</p>
                         <a href="" target="_self" rel="nofollow" class="btn btn-two px-3 py-1.5 rounded-full text-black bg-white mt-3" data-aos="flip-up" data-aos-duration="2000" style="border: 1px solid gray;">Learn More <i class="fa-solid fa-right-long text-primary-800"></i></a>
@@ -24,7 +24,7 @@
 
             <div class="flex flex-wrap items-center mt-10">
                 <div class="w-full lg:6/12 md:w-6/12">
-                    <div class="w-[80%]">
+                    <div class="w-full lg:w-[80%]">
                         <h3 class="text-3xl font-semibold text-secondary-800" data-aos="flip-up" data-aos-duration="2000">Collections</h3>
                         <p class="text-gray-700 mt-2 text-lg" data-aos="flip-up" data-aos-duration="2000">Our fraud proof collection and payment solution allow you to set up instant payments, while you get paid securely and promptly. Say goodbye to meeting tenants in-person and cashing paper checks.</p>
                         <a href="" target="_self" rel="nofollow" class="btn btn-two px-3 py-1.5 rounded-full text-black bg-white mt-3" data-aos="flip-up" data-aos-duration="2000" style="border: 1px solid gray;">Learn More <i class="fa-solid fa-right-long text-primary-800"></i></a>
@@ -38,7 +38,37 @@
                 </div>
             </div>
 
-           
+            <div class="flex flex-wrap items-center mt-10">
+                <div class="w-full lg:6/12 md:w-6/12">
+                    <div class="feature-card overflow-hidden" data-aos="flip-right" data-aos-duration="2000">
+                        <img src="../assets/access-control.png" alt="" class="mx-auto" >
+                    </div>
+                </div>
+
+                <div class="w-full lg:6/12 md:w-6/12">
+                    <div class="w-full lg:w-[80%]">
+                        <h3 class="text-3xl font-semibold text-secondary-800" data-aos="flip-up" data-aos-duration="2000">Access Control</h3>
+                        <p class="text-gray-700 mt-2 text-lg" data-aos="flip-up" data-aos-duration="2000">Create good feng shui in your spaces. Welcome guests and supercharges security with our automatic gate pass For Smart Vehicle Access Control.</p>
+                        <a href="" target="_self" rel="nofollow" class="btn btn-two px-3 py-1.5 rounded-full text-black bg-white mt-3" data-aos="flip-up" data-aos-duration="2000" style="border: 1px solid gray;">Learn More <i class="fa-solid fa-right-long text-primary-800"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap items-center mt-10">
+                <div class="w-full lg:6/12 md:w-6/12">
+                    <div class="w-full lg:w-[80%]">
+                        <h3 class="text-3xl font-semibold text-secondary-800" data-aos="flip-up" data-aos-duration="2000">Estate Management</h3>
+                        <p class="text-gray-700 mt-2 text-lg" data-aos="flip-up" data-aos-duration="2000">Run your Estate from any where and any device. Enjoy a suite of tools for reporting and managing concerns, & financial reporting with charts, and messaging, plus elections.</p>
+                        <a href="" target="_self" rel="nofollow" class="btn btn-two px-3 py-1.5 rounded-full text-black bg-white mt-3" data-aos="flip-up" data-aos-duration="2000" style="border: 1px solid gray;">Learn More <i class="fa-solid fa-right-long text-primary-800"></i></a>
+                    </div>
+                </div>
+
+                <div class="w-full lg:6/12 md:w-6/12">
+                    <div class="feature-card overflow-hidden" data-aos="flip-right" data-aos-duration="2000">
+                        <img src="../assets/estatemange.png" alt="" class="mx-auto" >
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
